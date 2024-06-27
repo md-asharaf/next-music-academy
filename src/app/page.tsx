ceea2d3c6fc4433233c1ccd54f5022ebe9a509f2
md asharaf <718romanempire@gmail.com>
2024-06-27 11:56:33 +0530

    solved Hero component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
-import Image from "next/image";
-
+import Hero from "@/components/Hero";
 export default function Home() {
     return (
         <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-            <h1 className="text-2xl mx-auto">Chai aur Code</h1>
+            <Hero />
         </main>
     );
 }
