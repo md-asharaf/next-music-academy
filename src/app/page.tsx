6286787808bb56cd401c691c11b6cd54fc6c5769
md asharaf <718romanempire@gmail.com>
2024-06-27 15:43:50 +0530

    chore: Add WhyChooseUs component to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import Hero from "@/components/Hero";
+import WhyChooseUs from "@/components/WhyChooseUs";
 export default function Home() {
     return (
         <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
             <Hero />
             <FeaturedCourses />
+            <WhyChooseUs />
         </main>
     );
 }
